d049af5a20af1814456c6543d8240690c5c9ef47
HarshitCoder2003 <152255166+HarshitCoder2003@users.noreply.github.com>
2023-12-19 18:12:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {  
+    strings: ["Frontened Developer", "Data Analyst",
+    "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
